721ad4f8dd30d02cf529d11925bfa23e8b297473
Nathanaël <88835356+RetroGoy@users.noreply.github.com>
2025-11-20 11:30:22 +0100

    Updated videos.js

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,9 +1,9 @@
 export const videos = [
   {
     id: 'video-1',
-    youtubeId: 'dQw4w9WgXcQ',
-    title: 'Court-métrage 1',
-    thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/mqdefault.jpg',
+    youtubeId: 'uMsgumidgy8',
+    title: 'Genocide de poulets',
+    thumbnail: 'https://img.youtube.com/vi/uMsgumidgy8/mqdefault.jpg',
     description: 'Un court-métrage explorant les thèmes de la mémoire et du temps à travers une narration visuelle poétique. Ce projet combine des techniques de prise de vue traditionnelles avec des effets visuels contemporains.',
     images: [
       'https://images.pexels.com/photos/2387418/pexels-photo-2387418.jpeg?auto=compress&cs=tinysrgb&w=800',
@@ -13,9 +13,9 @@ export const videos = [
   },
   {
     id: 'video-2',
-    youtubeId: 'dQw4w9WgXcQ',
-    title: 'Court-métrage 2',
-    thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/mqdefault.jpg',
+    youtubeId: 'j8EFK4OFf0Q',
+    title: 'Judason',
+    thumbnail: 'https://img.youtube.com/vi/j8EFK4OFf0Q/mqdefault.jpg',
     description: 'Une exploration visuelle des espaces urbains la nuit, capturant la solitude et la beauté des villes endormies. Réalisé entièrement en lumière naturelle et néons urbains.',
     images: [
       'https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?auto=compress&cs=tinysrgb&w=800',
@@ -25,9 +25,9 @@ export const videos = [
   },
   {
     id: 'video-3',
-    youtubeId: 'dQw4w9WgXcQ',
-    title: 'Motion Design',
-    thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/mqdefault.jpg',
+    youtubeId: 'RQWhuwgD9EU',
+    title: 'Regdumatum',
+    thumbnail: 'https://img.youtube.com/vi/RQWhuwgD9EU/mqdefault.jpg',
     description: 'Projet de motion design pour une identité de marque, intégrant des animations fluides et des transitions organiques. Créé avec After Effects et Cinema 4D.',
     images: [
       'https://images.pexels.com/photos/164821/pexels-photo-164821.jpeg?auto=compress&cs=tinysrgb&w=800',
@@ -37,9 +37,9 @@ export const videos = [
   },
   {
     id: 'video-4',
-    youtubeId: 'dQw4w9WgXcQ',
-    title: 'Animation 3D',
-    thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/mqdefault.jpg',
+    youtubeId: 'elOBRUXiOh4',
+    title: 'drone test',
+    thumbnail: 'https://img.youtube.com/vi/elOBRUXiOh4/mqdefault.jpg',
     description: 'Animation 3D abstraite explorant les formes géométriques et les textures organiques. Un voyage visuel à travers des paysages numériques générés de manière procédurale.',
     images: [
       'https://images.pexels.com/photos/1269968/pexels-photo-1269968.jpeg?auto=compress&cs=tinysrgb&w=800',
